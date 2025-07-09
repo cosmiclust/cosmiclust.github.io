18e9c6cd1a81d15fee13b64956e065b70127a8a0
महिमा <93143005+cosmiclust@users.noreply.github.com>
2025-07-09 22:08:18 +0530

    Create Home.py

---
+++ streamlitapp/Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="Home", page_icon="✨", layout="centered")
+
+st.title("Mahima Sahu")
+
+st.markdown(
+    """
+    > *Engineering builds on nature, inspired by it, driven by human purpose, and bound by nature's law.*
+    """,
+    unsafe_allow_html=True,
+)
+
+st.write(
+    "I am passionate about innovation, optimization, and complex precision backed with clarity and creativity."
+)
